e396b0553f3fcaa76464efd82e33ac1e17e46c4c
SufficientBeardage <55604951+SufficientBeardage@users.noreply.github.com>
2019-10-10 16:08:43 -0700

    added gameover audio
    Haven't tested due to compiler errors.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+//using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
     public void StartGame() {
-    	SceneManager.LoadScene ("RoomEscape");
+    	SceneManager.LoadScene("RoomEscape");
     }
 
     public void QuitGame() {
